946de3e0279d3bcc9dc85bda08665cb005a6ac94
Santiago Alegria <salegria@128technology.com>
2021-04-01 15:33:35 -0400

    Revert "Ouput and aggregator for Zabbix"
    This reverts commit d89bd9b2416c303d184f984dc8ab7dbb8ac844f9.

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -42,5 +42,4 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/outputs/warp10"
 	_ "github.com/influxdata/telegraf/plugins/outputs/wavefront"
 	_ "github.com/influxdata/telegraf/plugins/outputs/yandex_cloud_monitoring"
-	_ "github.com/influxdata/telegraf/plugins/outputs/zabbix"
 )
