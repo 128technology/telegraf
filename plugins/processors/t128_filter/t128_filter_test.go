34e2aaa5d64d064e8c4ca74ac9acba6c442a78fe
Benjamin Matase <bmatase@128technology.com>
2021-09-01 10:26:17 -0400

    Added operation config choice for each condition

--- plugins/processors/t128_filter/t128_filter_test.go
+++ plugins/processors/t128_filter/t128_filter_test.go
@@ -68,7 +68,7 @@ func TestFilters(t *testing.T) {
 			},
 		},
 		{
-			Name:       "ands tags together",
+			Name:       "ands tags together by default",
 			Conditions: []Condition{{Tags: tags{"tag1": {"value1"}, "tag2": {"value2"}}}},
 			InputMetrics: []telegraf.Metric{
 				newMetric("some-measurement", map[string]string{"tag1": "value1", "tag2": "value2"}, nil),
@@ -76,6 +76,21 @@ func TestFilters(t *testing.T) {
 				newMetric("some-measurement", map[string]string{"tag1": "value2", "tag2": "value2"}, nil),
 			},
 			OutputMetrics: []telegraf.Metric{newMetric("some-measurement", map[string]string{"tag1": "value1", "tag2": "value2"}, nil)},
+		},
+		{
+			Name:       "or operation ors tags together",
+			Conditions: []Condition{{Operation: orOperation, Tags: tags{"tag1": {"value1"}, "tag2": {"value2"}}}},
+			InputMetrics: []telegraf.Metric{
+				newMetric("some-measurement", map[string]string{"tag1": "value1", "tag2": "value2"}, nil),
+				newMetric("some-measurement", map[string]string{"tag1": "value1", "tag2": "value1"}, nil),
+				newMetric("some-measurement", map[string]string{"tag1": "value2", "tag2": "value2"}, nil),
+				newMetric("some-measurement", map[string]string{"tag1": "value2", "tag2": "value1"}, nil),
+			},
+			OutputMetrics: []telegraf.Metric{
+				newMetric("some-measurement", map[string]string{"tag1": "value1", "tag2": "value2"}, nil),
+				newMetric("some-measurement", map[string]string{"tag1": "value1", "tag2": "value1"}, nil),
+				newMetric("some-measurement", map[string]string{"tag1": "value2", "tag2": "value2"}, nil),
+			},
 		},
 		{
 			Name:       "ors multiple values",
@@ -152,6 +167,10 @@ func TestValidation(t *testing.T) {
 		{
 			Name:       "invalid mode",
 			Conditions: []Condition{{Mode: "some-invalid-mode", Tags: tags{"tag1": {"just needed a tag"}}}},
+		},
+		{
+			Name:       "invalid operation",
+			Conditions: []Condition{{Operation: "some-invalid-operation", Tags: tags{"tag1": {"just needed a tag"}}}},
 		},
 	}
 
