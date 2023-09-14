a4ea39c2a240f7feff5bf602b01525bfb840654f
Shriyansh Kothari <shriyanshk@juniper.net>
2023-09-14 14:18:59 -0400

    made some more enhancements
    WAN-2310 #time 1h

--- plugins/inputs/t128_lte/t128_lte.go
+++ plugins/inputs/t128_lte/t128_lte.go
@@ -1,13 +1,21 @@
 package t128_lte
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
 const (
-	ZK_LTE_STATE_PATH       = "/state/node/{node_name}/interface/{device_id}/network-plugin-state"
-	ZK_LTE_DEVICE_INFO_PATH = "network-plugin-info"
+	T128_CONFIG_FILE = "/var/run/128technology/config.json"
+	LTE_PATH_DIR     = "/var/run/128technology/lte"
+	GLOBAL_INIT_PATH = "/etc/128technology/global.init"
+	LOCAL_INIT_PATH  = "/etc/128technology/local.init"
 )
 
 var SERVICE_STATUS_MAP = map[string]string{
@@ -17,8 +25,13 @@ var SERVICE_STATUS_MAP = map[string]string{
 	"plmn-not-allowed":         "Forbidden PLMN",
 }
 
+var globalData map[string]interface{}
+var localData map[string]interface{}
+
 // T128Lte is an input for metrics of a 128T router instance
 type T128Lte struct {
+	RouterName string `toml:"router_name"`
+	NodeName   string `toml:"node_name"`
 }
 
 var sampleConfig = ``
@@ -34,11 +47,78 @@ func (*T128Lte) Description() string {
 }
 
 func (plugin *T128Lte) Init() error {
+	err := plugin.checkConfig()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (plugin *T128Lte) Gather(acc telegraf.Accumulator) error {
 
+	return nil
+}
+
+func (plugin *T128Lte) getRouterName() (interface{}, error) {
+	content, err := os.ReadFile(GLOBAL_INIT_PATH)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read the %s : %w", content, err)
+	}
+	err = json.Unmarshal(content, &globalData)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal %s : %w", content, err)
+	}
+	return globalData["routerName"], nil
+}
+
+func (plugin *T128Lte) getNodeName() (interface{}, error) {
+	content, err := os.ReadFile(LOCAL_INIT_PATH)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read the %s : %w", content, err)
+	}
+	err = json.Unmarshal(content, &globalData)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal %s : %w", content, err)
+	}
+	return globalData["routerName"], nil
+}
+
+func (plugin *T128Lte) getLteInterfaces() error {
+
+	return nil
+}
+
+func (plugin *T128Lte) getLteStateInfo(deviceName string) error {
+	stateFile := filepath.Join(LTE_PATH_DIR, fmt.Sprint(deviceName, ".state"))
+	fileInfo, err := plugin.checkFileExists(stateFile)
+	if err != nil {
+		return err
+	}
+	//Read the JSON File
+	return nil
+}
+
+func (plugin *T128Lte) getLteInfo(deviceName string) error {
+	infoFile := filepath.Join(LTE_PATH_DIR, fmt.Sprint(deviceName, ".info"))
+	fileInfo, err := plugin.checkFileExists(infoFile)
+	if err != nil {
+		return err
+	}
+	//Read the JSON File
+	return nil
+}
+
+func (plugin *T128Lte) checkFileExists(filePath string) (fs.FileInfo, error) {
+	checkFile, err := os.Stat(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("LTE %s doesn't exists : %w", filePath, err)
+	}
+	return checkFile, nil
+}
+
+func (plugin *T128Lte) checkConfig() error {
+	//Add the field to check if config path is present or not?
 	return nil
 }
 
