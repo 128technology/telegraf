a00bbd3b9125585ef5d3c489f98acef0ffb38b6f
Shriyansh Kothari <100544034+shriyanshk128T@users.noreply.github.com>
2024-07-29 10:18:17 -0400

    added a check for file already closed (#72)
    * added a check for file already closed
    
    WAN-3254 #time 2h
    
    * address comment
    
    WAN-3254 #time 30m

--- plugins/inputs/execd/execd.go
+++ plugins/inputs/execd/execd.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"strings"
 	"time"
 
@@ -140,6 +141,10 @@ func (e *Execd) cmdReadOutStream(out io.Reader) {
 			if err == influx.EOF {
 				break // stream ended
 			}
+			if errors.Is(err, os.ErrClosed) {
+				e.Log.Debug("ignoring stream closed")
+				break
+			}
 			if parseErr, isParseError := err.(*influx.ParseError); isParseError {
 				// parse error.
 				e.acc.AddError(parseErr)
@@ -162,6 +167,10 @@ func (e *Execd) cmdReadErr(out io.Reader) {
 	}
 
 	if err := scanner.Err(); err != nil {
+		if errors.Is(err, os.ErrClosed) {
+			e.Log.Debug("ignoring stderr: stream closed")
+			return
+		}
 		e.acc.AddError(fmt.Errorf("error reading stderr: %w", err))
 	}
 }
